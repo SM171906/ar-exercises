2403b94afb518ba096f7c8f4b487c1f199596052
Smiti <mishra.smiti06@gmail.com>
2021-11-08 18:13:37 -0500

    calculate total revenue, avg sale. total number of store >1M revenue

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,21 +4,21 @@ puts "Exercise 1"
 puts "----------"
 
 @store1 = Store.create(
-  "name": "Burnaby",
-  "annual_revenue": 300000, 
-  "mens_apparel": true,
-  "womens_apparel": true
+  name: "Burnaby",
+  annual_revenue: 300000, 
+  mens_apparel: true,
+  womens_apparel: true
 )
 @store2 = Store.create(
-  "name": "Richmond",
-  "annual_revenue": 1260000, 
-  "mens_apparel": false,
-  "womens_apparel": true
+  name: "Richmond",
+  annual_revenue: 1260000, 
+  mens_apparel: false,
+  womens_apparel: true
 )
 @store3 = Store.create(
-  "name": "Gastown",
-  "annual_revenue": 190000, 
-  "mens_apparel": true ,
-  "womens_apparel": false
+  name: "Gastown",
+  annual_revenue: 190000, 
+  mens_apparel: true ,
+  womens_apparel: false
 )
-puts "The total number of sotores in the database {Store.count}"
+puts "The total number of sotores in the database #{Store.count}"
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,10 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total = Store.sum(:annual_revenue)
+stores_number = Store.count
+puts "Total stores #{total}"
+
+puts "Average revenue for all sotres #{total/stores_number}"
+store_generating_more = Store.where("annual_revenue > 1000000").count
+puts "Store generating more than 1M #{store_generating_more}"
